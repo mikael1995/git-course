369bda9bc8fcd5c6721caaa7b1896dbd0b450d09
Mikael1995 <mikael.akopyan@gmail.com>
2022-03-27 23:48:21 +0300

    Practice 2

--- js/script.js
+++ js/script.js
@@ -10,13 +10,35 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов",""),
-      b = prompt("На сколько оцените его?",""),
-      c = prompt("Один из последних просмотренных фильмов",""),
-      d = prompt("На сколько оцените его?","");
+if (personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt("Один из последних просмотренных фильмов",""),
+//       b = prompt("На сколько оцените его?",""),
+//       c = prompt("Один из последних просмотренных фильмов",""),
+//       d = prompt("На сколько оцените его?","");
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов",""),
+          b = prompt("На сколько оцените его?","");
+          
+          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+          } else {
+              console.log('Error');
+              i--;
+          }
+    
+          
+}
 
 
 
